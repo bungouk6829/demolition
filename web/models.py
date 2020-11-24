080e50ae1fa7fee6ca351e471e5b8931a5736a9a
ByeongUkKim <bungouk6829@gmail.com>
2020-11-25 08:19:23 +0900

    10th_commit

--- web/models.py
+++ web/models.py
@@ -51,6 +51,17 @@ class Information_post(models.Model):
     text = models.TextField(max_length=5000)
     phone_number = models.CharField(max_length=20)
     create_at = models.DateField(auto_now_add=True)
+    clicks = models.IntegerField(default=1)
+    file_1 = models.FileField(upload_to='files/', null=True, blank=True)
+    file_2 = models.FileField(upload_to='files/', null=True, blank=True)
+    file_3 = models.FileField(upload_to='files/', null=True, blank=True)
+    file_4 = models.FileField(upload_to='files/', null=True, blank=True)
+    file_5 = models.FileField(upload_to='files/', null=True, blank=True)
+
+    @property
+    def update_clicks(self):
+        self.clicks += 1
+        self.save()
 
     def __str__(self):
         return self.title
--- web/views.py
+++ web/views.py
@@ -34,16 +34,39 @@ def menu_6(request, page):
 		return render(request, 'web/menu_6.html', {'page':page,'notice_posts':notice_posts})
 
 	elif page == 'information':
-		pass
+		information_posts_all = Information_post.objects.all().order_by('-id')
+		page_number = int(request.GET.get('p', 1))
+		pagenator = Paginator(information_posts_all, 11)
+		information_posts = pagenator.get_page(page_number)
+		return render(request, 'web/menu_6.html', {'page':page,'information_posts':information_posts})
 
-def detail_notice(request, page, notice_post_pk):
+def detail_post(request, page, post_pk):
 	if page == 'notice':
-		notice_post = get_object_or_404(Notice_post, pk=notice_post_pk)
+		notice_post = get_object_or_404(Notice_post, pk=post_pk)
 		return render(request, 'web/detail_notice.html', {'notice_post':notice_post})
 
-	else:
-		pass
+	elif page == 'information':
+		success = 0
+		name = 'enter'
+		information_post = get_object_or_404(Information_post, pk=post_pk)
+		if request.POST['post_password'] == information_post.password:
+			return render(request, 'web/detail_information.html', {'page':page,'information_post':information_post})
+		return render(request, 'web/check_password_information_post.html', {'page':page,'success':success,'name':'enter'})
 
 def detail_result(request, result_post_pk):
 	result_post = get_object_or_404(Result_post, pk=result_post_pk)
 	return render(request, 'web/detail_result.html', {'result_post':result_post})
+
+def edit_information(request, page, post_pk, name):
+	success = 0
+	if page == 'information':
+		information_post = get_object_or_404(Information_post, pk=post_pk)
+		if name == 'delete':
+			if request.POST['post_password'] == information_post.password:
+				information_post.delete()
+				success = 1
+	return render(request, 'web/check_password_information_post.html', {'name':name,'page':page,'success':success})
+
+def input_information_post_password(request, page, post_pk, name):
+	information_post = get_object_or_404(Information_post, pk=post_pk)
+	return render(request, 'web/input_information_post_password.html', {'page':page,'information_post':information_post,'name':name})
